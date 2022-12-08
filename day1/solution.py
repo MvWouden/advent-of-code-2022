9027a2796dfb0e2abb3ac21326d332f9bb1f7a42
Milan van Wouden <milanvanwouden@live.nl>
2022-12-08 19:19:50 +0100

    day1

---
+++ day1/solution.py
@@ -0,0 +1,18 @@
+"""
+Advent of Code 2022: day 1
+"""
+calories = [
+    sum([                                # Sum up cluster
+        int(calories)                    # Transform to int
+        for calories in elf.split('\n')  # Split single caloric items
+        if calories                      # Ignore empty string
+    ])
+    for elf in open('input.txt').read().split('\n\n')  # Divide input into clusters
+]
+calories.sort()
+
+# Reverse and take top 3
+top3 = calories[:-4:-1]
+
+print(f'part1: {top3[0]}')
+print(f'part2: {sum(top3)}')
